75750b7a6fe4e42359d8a5b9c18053e519a2756a
XuTongle <xutongle@gmail.com>
2021-05-27 01:24:23 +0800

    c

--- src/HasCensor.php
+++ src/HasCensor.php
@@ -9,15 +9,20 @@ namespace Larva\Censor;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Support\Facades\Event;
+use Larva\Censor\Models\ContentCensor;
 
 /**
  * 内容审查
  * @property boolean $status 状态：0 待审核 1 审核通过 2 拒绝 3 推迟审核
+ * @property ContentCensor $stopWords 触发的审核词
+ *
  * @method static Builder approve() 审核通过的
  * @method static Builder pending() 待审核的
  * @method static Builder postponed() 被推迟的
  * @method static Builder rejected() 审核拒绝的
+ *
  * @mixin  Model
  * @author Tongle Xu <xutongle@gmail.com>
  */
@@ -27,6 +32,16 @@ trait HasCensor
      * @var string
      */
     protected $statusField = 'status';
+
+    /**
+     * Get the entity's stopWords.
+     *
+     * @return MorphOne
+     */
+    public function stopWords(): MorphOne
+    {
+        return $this->morphOne(ContentCensor::class, 'source');
+    }
 
     /**
      * 查询待审核的
@@ -74,9 +89,8 @@ trait HasCensor
      */
     public function markApproved(): bool
     {
-        $status = $this->forceFill([
-            $this->statusField => Status::APPROVED,
-        ])->save();
+        $this->status = Status::APPROVED;
+        $status = $this->saveQuietly();
         Event::dispatch(new Events\CensorApproved($this));
         return $status;
     }
@@ -87,9 +101,8 @@ trait HasCensor
      */
     public function markPostponed(): bool
     {
-        $status = $this->forceFill([
-            $this->statusField => Status::POSTPONED,
-        ])->save();
+        $this->status = Status::POSTPONED;
+        $status = $this->saveQuietly();
         Event::dispatch(new Events\CensorPostponed($this));
         return $status;
     }
@@ -100,9 +113,8 @@ trait HasCensor
      */
     public function markPending(): bool
     {
-        $status = $this->forceFill([
-            $this->statusField => Status::PENDING,
-        ])->save();
+        $this->status = Status::PENDING;
+        $status = $this->saveQuietly();
         Event::dispatch(new Events\CensorPending($this));
         return $status;
     }
@@ -113,10 +125,35 @@ trait HasCensor
      */
     public function markRejected(): bool
     {
-        $status = $this->forceFill([
-            $this->statusField => Status::REJECTED,
-        ])->save();
+        $this->status = Status::REJECTED;
+        $status = $this->saveQuietly();
         Event::dispatch(new Events\CensorRejected($this));
         return $status;
+    }
+
+    /**
+     * 获取状态Label
+     * @return string[]
+     */
+    public static function getStatusLabels(): array
+    {
+        return [
+            Status::PENDING => '待审核',
+            Status::APPROVED => '已审核',
+            Status::REJECTED => '拒绝',
+        ];
+    }
+
+    /**
+     * 获取状态Dot
+     * @return string[]
+     */
+    public static function getStatusDots(): array
+    {
+        return [
+            Status::PENDING => 'info',
+            Status::APPROVED => 'success',
+            Status::REJECTED => 'error',
+        ];
     }
 }
